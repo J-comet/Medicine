e9aba9bea94651e6b62f636fdf096cdfcf604f02
Jang <comet@theboms.co.kr>
2021-12-09 17:35:37 +0900

    사용자 추가화면 UI 작업 중간저장

--- app/src/main/java/hs/project/medicine/activitys/AddUserActivity.java
+++ app/src/main/java/hs/project/medicine/activitys/AddUserActivity.java
@@ -1,16 +1,30 @@
 package hs.project.medicine.activitys;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
 
+import com.google.android.material.textfield.TextInputLayout;
+
+import hs.project.medicine.MediApplication;
 import hs.project.medicine.R;
 
 public class AddUserActivity extends AppCompatActivity {
+
+    private TextInputLayout tilName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_user);
+
+        init();
+
+    }
+
+    private void init() {
+        tilName = findViewById(R.id.til_name);
+        tilName.setStartIconTintList(ContextCompat.getColorStateList(MediApplication.ApplicationContext(), R.color.selector_starticon));
     }
 }
