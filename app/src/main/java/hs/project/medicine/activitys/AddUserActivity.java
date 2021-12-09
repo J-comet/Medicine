a6e8f4af96635f0848f3df1f0ba96c566e4d0bdc
Jang <comet@theboms.co.kr>
2021-12-09 17:21:33 +0900

    사용자 추가화면 UI 작업 중간저장

---
+++ app/src/main/java/hs/project/medicine/activitys/AddUserActivity.java
@@ -0,0 +1,16 @@
+package hs.project.medicine.activitys;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import hs.project.medicine.R;
+
+public class AddUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_user);
+    }
+}
--- app/src/main/java/hs/project/medicine/activitys/MainActivity.java
+++ app/src/main/java/hs/project/medicine/activitys/MainActivity.java
@@ -6,7 +6,9 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import hs.project.medicine.R;
 import hs.project.medicine.custom_view.MainBottomView;
@@ -14,6 +16,9 @@ import hs.project.medicine.custom_view.MainBottomView;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private MainBottomView mainBottomView;
+
+    private LinearLayout liAddUser;
+    private LinearLayout liMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +31,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private void init() {
         mainBottomView = findViewById(R.id.main_bottom_view);
+        liMenu = findViewById(R.id.li_menu);
+        liAddUser = findViewById(R.id.li_add_user);
+
+        liMenu.setOnClickListener(this);
+        liAddUser.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
 
         switch (v.getId()) {
+            case R.id.li_menu:
+                Toast.makeText(this,"메뉴클릭", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.li_add_user:
+                Intent intent = new Intent(MainActivity.this, AddUserActivity.class);
+                startActivity(intent);
+                break;
+
            /* case R.id.cl_search_medic:
                 Intent intentMoveSearchMedic = new Intent(MainActivity.this, SearchMedicineActivity.class);
                 intentMoveSearchMedic.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
