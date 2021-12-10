f6fdfd74ad8c6a6bf3c75372c188b83d42436df4
Jang <comet@theboms.co.kr>
2021-12-10 09:55:15 +0900

    사용자 추가화면 클릭리스너 추가

--- app/src/main/java/hs/project/medicine/activitys/AddUserActivity.java
+++ app/src/main/java/hs/project/medicine/activitys/AddUserActivity.java
@@ -4,15 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputLayout;
+
+import org.w3c.dom.Text;
 
 import hs.project.medicine.MediApplication;
 import hs.project.medicine.R;
 
-public class AddUserActivity extends AppCompatActivity {
+public class AddUserActivity extends AppCompatActivity implements View.OnClickListener{
 
-    private TextInputLayout tilName;
+//    private TextInputLayout tilName;
+
+    private EditText etName;
+    private TextView tvGender;
+    private TextView tvAge;
+    private LinearLayout liBack;
+    private LinearLayout liComplete;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +37,36 @@ public class AddUserActivity extends AppCompatActivity {
     }
 
     private void init() {
+        etName = findViewById(R.id.et_name);
+        tvGender = findViewById(R.id.tv_gender);
+        tvAge = findViewById(R.id.tv_age);
+        liBack = findViewById(R.id.li_back);
+        liComplete = findViewById(R.id.li_complete);
+
+        tvGender.setOnClickListener(this);
+        tvAge.setOnClickListener(this);
+        liBack.setOnClickListener(this);
+        liComplete.setOnClickListener(this);
+
 //        tilName = findViewById(R.id.til_name);
 //        tilName.setStartIconTintList(ContextCompat.getColorStateList(MediApplication.ApplicationContext(), R.color.selector_starticon));
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.tv_gender:
+                Toast.makeText(this, "성별", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.tv_age:
+                Toast.makeText(this, "나이", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.li_back:
+                finish();
+                break;
+            case R.id.li_complete:
+                Toast.makeText(this, "등록하기", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
