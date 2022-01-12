5bf233711fe92fbbefe94216875e8b8be271c2f3
hyeseong <hyeseong7848@gmail.com>
2022-01-12 20:54:59 +0900

    UI 수정 및 로딩 로티 애니매이션 추가

--- app/src/main/java/hs/project/medicine/dialog/BottomSheetMapSearchDialog.java
+++ app/src/main/java/hs/project/medicine/dialog/BottomSheetMapSearchDialog.java
@@ -47,10 +47,13 @@ public class BottomSheetMapSearchDialog extends BottomSheetDialogFragment implem
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = LayoutMapSearchSheetBinding.inflate(LayoutInflater.from(context), container, false);
-
-        init();
-
         return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        init();
     }
 
     private void init() {
