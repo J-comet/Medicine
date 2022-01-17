1ad4a1d11c377b5cff3ec6f8508be0b1c0565366
hyeseong <hyeseong7848@gmail.com>
2022-01-17 21:43:21 +0900

    MainUserListView 추가

--- app/src/main/java/hs/project/medicine/main_content/MainUserListView.java
+++ app/src/main/java/hs/project/medicine/main_content/MainUserListView.java
@@ -9,36 +9,36 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
-import hs.project.medicine.databinding.LayoutMainTvViewBinding;
+import hs.project.medicine.databinding.LayoutMainUserListViewBinding;
 
-public class MainTvView extends ConstraintLayout implements View.OnClickListener {
-    private LayoutMainTvViewBinding binding;
+public class MainUserListView extends ConstraintLayout implements View.OnClickListener {
+    private LayoutMainUserListViewBinding binding;
     private Context context;
 
-    public MainTvView(@NonNull Context context) {
+    public MainUserListView(@NonNull Context context) {
         super(context);
         this.context = context;
         initView(context);
     }
 
-    public MainTvView(@NonNull Context context, @Nullable AttributeSet attrs) {
+    public MainUserListView(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         this.context = context;
         initView(context);
     }
 
-    public MainTvView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+    public MainUserListView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         this.context = context;
         initView(context);
     }
 
-    public MainTvView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+    public MainUserListView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
     }
 
     private void initView(Context context) {
-        binding = LayoutMainTvViewBinding.inflate(LayoutInflater.from(context), this, true);
+        binding = LayoutMainUserListViewBinding.inflate(LayoutInflater.from(context), this, true);
     }
 
     @Override
