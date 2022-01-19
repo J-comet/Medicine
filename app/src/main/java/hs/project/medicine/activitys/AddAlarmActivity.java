5606618a31db13315ffc0a022329b5247ce7691e
hyeseong <hyeseong7848@gmail.com>
2022-01-19 15:43:16 +0900

    요일 선택 View 선택때 백그라운드 변경 추가

--- app/src/main/java/hs/project/medicine/activitys/AddAlarmActivity.java
+++ app/src/main/java/hs/project/medicine/activitys/AddAlarmActivity.java
@@ -4,6 +4,8 @@ import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.NumberPicker;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -27,6 +29,21 @@ public class AddAlarmActivity extends BaseActivity implements View.OnClickListen
 
     private void init() {
         binding.liBack.setOnClickListener(this);
+        binding.tvSunday.setOnClickListener(this);
+        binding.tvMonday.setOnClickListener(this);
+        binding.tvTuesday.setOnClickListener(this);
+        binding.tvWednesday.setOnClickListener(this);
+        binding.tvThursday.setOnClickListener(this);
+        binding.tvFriday.setOnClickListener(this);
+        binding.tvSaturday.setOnClickListener(this);
+
+        binding.tvSunday.setSelected(true);
+        binding.tvMonday.setSelected(true);
+        binding.tvTuesday.setSelected(true);
+        binding.tvWednesday.setSelected(true);
+        binding.tvThursday.setSelected(true);
+        binding.tvFriday.setSelected(true);
+        binding.tvSaturday.setSelected(true);
 
         String[] arrAmPm = getResources().getStringArray(R.array.arr_am_pm);
 
@@ -50,11 +67,83 @@ public class AddAlarmActivity extends BaseActivity implements View.OnClickListen
         binding.npMinute.setDisplayedValues(array);
     }
 
+    private void dayOfWeekStatus(boolean isSelected, TextView textView) {
+        if (isSelected) {
+            textView.setSelected(true);
+        } else {
+            textView.setSelected(false);
+        }
+    }
+
+    private void resultDayOfWeek(boolean sunday, boolean monday, boolean tuesday, boolean wednesday, boolean thursday, boolean friday, boolean saturday) {
+        String result = "";
+
+        if (sunday && monday && tuesday && wednesday && thursday && friday && saturday) {
+            result = "매일";
+        } else if (monday && tuesday && wednesday && thursday && friday) {
+            result = "주중";
+        } else if (sunday && saturday) {
+            result = "주말";
+        } else {
+            result = "none";
+        }
+
+    }
+
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.li_back:
                 finish();
+                break;
+            case R.id.tv_sunday:
+                if (binding.tvSunday.isSelected()) {
+                    dayOfWeekStatus(false, binding.tvSunday);
+                } else {
+                    dayOfWeekStatus(true, binding.tvSunday);
+                }
+                break;
+            case R.id.tv_monday:
+                if (binding.tvMonday.isSelected()) {
+                    dayOfWeekStatus(false, binding.tvMonday);
+                } else {
+                    dayOfWeekStatus(true, binding.tvMonday);
+                }
+                break;
+            case R.id.tv_tuesday:
+                if (binding.tvTuesday.isSelected()) {
+                    dayOfWeekStatus(false, binding.tvTuesday);
+                } else {
+                    dayOfWeekStatus(true, binding.tvTuesday);
+                }
+                break;
+            case R.id.tv_wednesday:
+                if (binding.tvWednesday.isSelected()) {
+                    dayOfWeekStatus(false, binding.tvWednesday);
+                } else {
+                    dayOfWeekStatus(true, binding.tvWednesday);
+                }
+                break;
+            case R.id.tv_thursday:
+                if (binding.tvThursday.isSelected()) {
+                    dayOfWeekStatus(false, binding.tvThursday);
+                } else {
+                    dayOfWeekStatus(true, binding.tvThursday);
+                }
+                break;
+            case R.id.tv_friday:
+                if (binding.tvFriday.isSelected()) {
+                    dayOfWeekStatus(false, binding.tvFriday);
+                } else {
+                    dayOfWeekStatus(true, binding.tvFriday);
+                }
+                break;
+            case R.id.tv_saturday:
+                if (binding.tvSaturday.isSelected()) {
+                    dayOfWeekStatus(false, binding.tvSaturday);
+                } else {
+                    dayOfWeekStatus(true, binding.tvSaturday);
+                }
                 break;
         }
     }
